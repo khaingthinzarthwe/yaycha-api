f43f103fb40219675ec67e6a7bdd8f186cd1f888
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-08-05 19:51:10 +0700

    Creat Dynamic Route

--- routers/content.js
+++ routers/content.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const prisma = require('../PrismaClient');
+const prisma = require("../PrismaClient");
 
-router.get('/posts', async (req,res) => {
-    try{
-        const data = await prisma.post.findMany({
-            include: {
-                user: true,
-                comments:true,
-            },
-            orderBy: { id: "desc" },
-            take: 20,
-        });
-        res.json(data);
-    } catch (e) {
-        res.status(500).json({ error: e });
-    }
+router.get("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const data = await prisma.post.findFirst({
+      where: { id: Number(id) },
+      include: {
+        user: true,
+        comments: {
+          include: { user: true },
+        },
+      },
+    });
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 });
 
 module.exports = { contentRouter: router };
