b71a8b2a3401ff90eb0fa9967d6b98d8d969ecea
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-08-05 20:32:31 +0700

    Delete Content Post

--- routers/content.js
+++ routers/content.js
@@ -3,23 +3,24 @@ const router = express.Router();
 
 const prisma = require("../PrismaClient");
 
-router.get("/posts/:id", async (req, res) => {
+router.delete("/posts/:id", async (req, res) => {
   const { id } = req.params;
 
-  try {
-    const data = await prisma.post.findFirst({
-      where: { id: Number(id) },
-      include: {
-        user: true,
-        comments: {
-          include: { user: true },
-        },
-      },
-    });
-    res.json(data);
-  } catch (e) {
-    res.status(500).json({ error: e });
-  }
+  await prisma.comment.deleteMany({
+    where: { postId: Number(id) },
+  });
+  await prisma.post.delete({
+    where: { id: Number(id) },
+  });
+  res.sendStatus(204);
+});
+
+router.delete("/comments/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.comment.delete({
+    where: { id: Number(id) },
+  });
+  res.sendStatus(204);
 });
 
 module.exports = { contentRouter: router };
